71a05cbdb9c7362432ea00ea1b3008b57edd59fd
Roberto Aguiar Lima <aguiarlima.roberto@gmail.com>
2014-10-03 20:41:51 -0300

    Mapping Sensory cells to binary m

--- cmac.py
+++ cmac.py
@@ -1,12 +1,78 @@
-class Input(object):
-	def __init__(self, index, desc, min_val, max_val):
-		self._index = index
-		self._min_val = min_val
-		self._max_val = max_val
-		self._desc = desc
-	@property
-	def index(self):
-		return self._index
+from numpy import *
+import math
+
+class CMAC(object):
+    def __init__(self, s_min, s_max, num_possible_values, num_active_cells):
+        self._s_min = s_min
+        self._s_max = s_max
+        self._num_possible_values = num_possible_values
+        self._num_active_cells = num_active_cells
+        self._set_mapping_address()
+        self._set_mapping()
+
+    def _set_mapping(self):
+        temp_map = []
+        i = 0
+        qtd = 0
+        stop = False
+        while True:
+            temp = []
+            for j in range(self.num_active_cells):
+                line = []
+                for k in range(j, self.num_active_cells):
+                    line.append(k)
+                temp.append(line)
+                qtd = qtd + 1
+                if qtd == self.num_possible_values:
+                    stop = True
+                    break 
+            for l in range(len(temp)):
+               for m in range(len(temp[l])):
+                    temp[l][m] = temp[l][m] + i
+            acc = 0 
+            for j in range(self.num_active_cells):
+                if stop and j >= len(temp):
+                    break;
+                if j == 0:
+                    acc = temp[0][-1]
+                for k in range(self.num_active_cells - len(temp[j])):
+                    temp[j].insert(k, acc+k+1)
+                    
+            temp_map.extend(temp)    
+            if stop: break
+            i = i + self.num_active_cells 
+
+        self._mapping = array(temp_map)
+
+    def _set_mapping_address(self):
+        self._mapping_address = linspace(self._s_min, self._s_max, self._num_possible_values)
+
+    @property
+    def mapping(self):
+        return self._mapping
+    @property
+    def mapping_address(self):
+        return self._mapping_address
+    @property
+    def num_active_cells(self):
+        return self._num_active_cells
+    @property
+    def num_possible_values(self):
+        return self._num_possible_values
+    @property
+    def s_min(self):
+        return self._s_min
+    @property
+    def s_max(self):
+        return self._s_max
+
+                
+
+class CMACLegProsthesis(object):
+	def __init__(self, active_sensory_cells):
+		self._active_sensory_cells = active_sensory_cells
+
+	def train(self, data_loader): pass
 
 class DataLoader(object):
 	def __init__(self, inputs, output_indexes):
@@ -59,12 +125,17 @@ class DataLoader(object):
 	@property
 	def data(self):
 		return self._data
-	
-class CMACLegProsthesis(object):
-	def __init__(self, active_sensory_cells):
-		self._active_sensory_cells = active_sensory_cells
 
-	def train(self, data_loader): pass
+class Input(object):
+	def __init__(self, index, desc, min_val, max_val):
+		self._index = index
+		self._min_val = min_val
+		self._max_val = max_val
+		self._desc = desc
+	@property
+	def index(self):
+		return self._index
+
 
 def generate_inputs():
 	inputs = []
--- test_cmac.py
+++ test_cmac.py
@@ -3,6 +3,44 @@ import cmac
 
 class TestCmac(unittest.TestCase):
     def setUp(self):
+        self._cmac = cmac.CMAC(0., 12., 13, 4)
+
+    def tearDown(self):
+        self._cmac = None
+
+    def test_mapping_address_first_value(self):
+            self.assertTrue(self._cmac.mapping_address[0] == self._cmac.s_min)
+
+    def test_mapping_address_last_value(self):
+            self.assertTrue(self._cmac.mapping_address[-1] == self._cmac.s_max)
+   
+    def test_mapping_address_size(self):
+            self.assertTrue(len(self._cmac.mapping_address) == 13)
+
+
+    def test_mapping_all_lines(self):
+        self.assertTrue(self._cmac.mapping[0].tolist() == [0, 1, 2, 3])
+        self.assertTrue(self._cmac.mapping[1].tolist() == [4, 1, 2, 3])
+        self.assertTrue(self._cmac.mapping[2].tolist() == [4, 5, 2, 3])
+        self.assertTrue(self._cmac.mapping[3].tolist() == [4, 5, 6, 3])
+        self.assertTrue(self._cmac.mapping[4].tolist() == [4, 5, 6, 7])
+        self.assertTrue(self._cmac.mapping[5].tolist() == [8, 5, 6, 7])
+        self.assertTrue(self._cmac.mapping[6].tolist() == [8, 9, 6, 7])
+        self.assertTrue(self._cmac.mapping[7].tolist() == [8, 9, 10, 7])
+        self.assertTrue(self._cmac.mapping[8].tolist() == [8, 9, 10, 11])
+        self.assertTrue(self._cmac.mapping[9].tolist() == [12, 9, 10, 11])
+        self.assertTrue(self._cmac.mapping[10].tolist() == [12, 13, 10, 11])
+        self.assertTrue(self._cmac.mapping[11].tolist() == [12, 13, 14, 11])
+        self.assertTrue(self._cmac.mapping[12].tolist() == [12, 13, 14, 15])
+ 
+
+    def test_mapping_size(self):
+        self.assertTrue(self._cmac.mapping.shape == (self._cmac.num_possible_values, self._cmac.num_active_cells))
+
+ 
+
+class TestCmacLegProsthesis(unittest.TestCase):
+    def setUp(self):
         inputs = []
 	for i in range(9):
 		inputs.append(cmac.Input(i, 'Attr ' + str(i), -10, 10))
@@ -35,4 +73,6 @@ class TestDataLoader(unittest.TestCase):
         self.assertTrue(len(self.data_loader.output_data) > 0, 'Output data not loaded')
         self.assertTrue(len(self.data_loader.output_data[0]) == 3, 'Number of outputs wrong!')
 
-    
+
+if __name__ == '__main__':
+    unittest.main()
