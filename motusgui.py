8c36148826629f04cee2f91650cddc24c740923c
Roberto Aguiar Lima <robertoaguiarlima@imac.Home>
2014-10-31 20:16:24 -0200

    More gui

--- motusgui.py
+++ motusgui.py
@@ -5,6 +5,18 @@ class App(object):
         self._root.geometry('800x600')
         self._commands = Commands()
         self._generate_menu_bar()
+        self._create_cmac_frame()
+
+    def _create_cmac_frame(self):
+        f = Frame(self._root)
+        f.pack(anchor=W)
+        Label(f, text = 'Description').grid(row=0, column=0, sticky=E)
+        Label(f, text = 'Activations').grid(row=1, column=0, sticky=E)
+        Label(f, text = 'Iterations').grid(row=2, column=0, sticky=E)
+
+        Entry(f).grid(row=0, column=1, sticky=W)
+        Spinbox(f, from_=1, to=30).grid(row=1, column=1, sticky=W)
+        Spinbox(f, from_=1, to=200).grid(row=2, column=1, sticky=W)
     
     def _generate_menu_bar(self):
         menu_bar = Menu(self._root)
