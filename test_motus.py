b3b002dcba4241fc2835733317b1b8ff09e8eb80
Roberto Aguiar Lima <robertoaguiarlima@imac.Home>
2014-11-06 01:44:07 -0200

    testing

---
+++ test_motus.py
@@ -0,0 +1,12 @@
+import unittest;
+import motus;
+
+class MotusTest(unittest.TestCase):
+    def test_validate_description(self):
+        self.assertRaises(motus.ParameterInvalid, motus.Motus, desc='') 
+
+    def test_validate_acttivations(self):
+        self.assertRaises(motus.ParameterInvalid, motus.Motus, desc='aaa', activations=0)
+    
+    def test_validade_configurations(self):
+        self.assertRaises(motus.ParameterInvalid, motus.Motus, desc='bbb', activations = 4, configs=[(5, 3)])
